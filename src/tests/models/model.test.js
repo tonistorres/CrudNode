59a5581308fe6596f65d026b2b6556808be7e802
tonistorres <sisvenda2011@gmail.com>
2022-04-07 14:41:30 -0300

    test: implementando test funcao getAll camada model

--- src/tests/models/model.test.js
+++ src/tests/models/model.test.js
@@ -5,6 +5,26 @@ const UserModel = require("../../models/data.user");
 
 describe("Users Model", () => {
 
+
+  describe("Testando função getAllModel Se é EXECUTADA", () => {
+   
+    const returnDBGetAllModel = [[]];
+   
+    before(() => {
+      sinon.stub(connection, "execute").resolves(returnDBGetAllModel);
+    });
+
+    after(() => {
+      connection.execute.restore();
+    });
+
+    it('Testar se a função getAllModel é executada', async () => {
+      await UserModel.getAllModel();
+      sinon.assert.called(connection.execute);
+    });
+  });
+
+
   describe("Testando função getAllModel Qdo NAO ENCONTRAR registros no DB", () => {
    
     const returnDBGetAllModel = [[]];
@@ -15,6 +35,11 @@ describe("Users Model", () => {
 
     after(() => {
       connection.execute.restore();
+    });
+
+    it('Testar se a função getAllModel é executada', async () => {
+      await UserModel.getAllModel();
+      sinon.assert.called(connection.execute);
     });
 
     it("Qdo NAO houver resgistros cadastrados no Data Base", async () => {
